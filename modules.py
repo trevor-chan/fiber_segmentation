f9b6cce5f7879b82f5ab4d8b1b953214422b3ab3
trevor-chan <tjchan0@gmail.com>
2020-08-12 07:54:29 -0700

    fixes to nms

--- modules.py
+++ modules.py
@@ -203,6 +203,7 @@ def polygon_nms(instances, score_threshold = .7, top_k=10000, nms_threshold = .5
     from nms_altered import nms
     polygons = instances.pred_masks
     print(type(polygons))
+    print(type(polygons[0])
     polygons = [np.reshape(polygon,(2,int(polygon.size()/2))) for polygon in polygons]
                 # pass list of lists of polygon vertices
     scores = instances.scores
