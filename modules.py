c8e2e338de67ca35ff053de15c7ca9e938c57c14
trevor-chan <tjchan0@gmail.com>
2020-08-13 10:25:13 -0700

    fixes to nms

--- modules.py
+++ modules.py
@@ -201,9 +201,12 @@ def nms(instances, overlap=0.5, top_k=10000):
 
 def polygon_nms(instances, score_threshold = .7, top_k=10000, nms_threshold = .5):
     from nms_altered import nms
+    
+    def choose_larger(poly):
+        return max(poly, key = lambda i: len(i))
     polygons = instances.pred_masks
     polygons = [np.reshape(polygon,(int(len(np.transpose(polygon))/2),2)) for polygon in polygons]
-    polygons = [poly if len(poly) < 2 else poly[0] for poly in polygons]
+    polygons = [poly if len(poly) < 2 else choose_larger(poly) for poly in polygons]
                 # pass list of lists of polygon vertices
     
     scores = instances.scores
