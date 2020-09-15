628cd87f881d8cd5c9adb6954081ee42167e201f
Trevor Chan <tjc54@grace1.grace.hpc.yale.internal>
2020-09-15 10:17:35 -0400

    added batch script for gpu submission job training

---
+++ model_train.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=fibersegTrain
+#SBATCH --out="slurm-%j.out"
+#SBATCH --time=40:00:00
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=1
+#SBATCH --mem-per-cpu=8G
+#SBATCH --mail-type=ALL
+#SBATCH --gres=gpu:1
+#SBATCH --requeue
+#SBATCH --partition=gpu
+
+module load CUDA
+module load cuDNN
+
+source activate genv
+
+python train.py
